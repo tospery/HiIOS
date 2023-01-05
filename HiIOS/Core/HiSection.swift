68bac92afc5208514d0178fbbc0db7d2cf9a965b
YangJianxiang <tospery@gmail.com>
2023-01-05 12:34:28 +0800

    section

---
+++ HiIOS/Core/HiSection.swift
@@ -0,0 +1,15 @@
+//
+//  HiSection.swift
+//  HiIOS
+//
+//  Created by 杨建祥 on 2023/1/5.
+//
+
+import Foundation
+
+struct HiSection {
+
+    let header: ModelType?
+    let models = [ModelType].init()
+
+}
