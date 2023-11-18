d687b5f4eafe5a1848fde8179d357d85025ca464
tospery <tospery@gmail.com>
2023-11-18 14:26:18 +0800

    HiIOSTmpl

---
+++ Examples/HiIOSCore/HiIOSCore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HiIOSCore
+//
+//  Created by 杨建祥 on 2023/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Examples/HiIOSTmpl/HiIOSTmpl/OC-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  OC-Bridging-Header.h
+//  HiIOSTmpl
+//
+//  Created by 杨建祥 on 2023/11/18.
+//
+
+#ifndef OC_Bridging_Header_h
+#define OC_Bridging_Header_h
+
+#import "OCHelper.h"
+
+#endif /* OC_Bridging_Header_h */
