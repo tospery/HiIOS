363eed0d64678ec1cba9d11f0060058863231577
tospery <tospery@gmail.com>
2023-11-23 15:05:26 +0800

    swiftlint

---
+++ Examples/HiIOSTmpl/HiIOSTmpl/OC-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  OC-Bridging-Header.h
+//  HiIOSTmpl
+//
+//  Created by 杨建祥 on 2023/11/23.
+//
+
+#ifndef OC_Bridging_Header_h
+#define OC_Bridging_Header_h
+
+
+#endif /* OC_Bridging_Header_h */
