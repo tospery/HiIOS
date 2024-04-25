a7c41bfbc087a4abc5949f8f30f6ce1feb90fd1e
tospery <tospery@gmail.com>
2024-04-25 12:50:46 +0800

    url

--- HiIOS/Core/Ex/URL+Core.swift
+++ HiIOS/Core/Ex/URL+Core.swift
@@ -19,23 +19,49 @@ public extension URL {
     }
     
     func myAppendingQueryParameters(_ parameters: [String: String]) -> URL {
-        guard var components = URLComponents.init(url: self, resolvingAgainstBaseURL: true) else { return self }
-        var items = components.queryItems ?? []
+        var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+        var queryItems = urlComponents.queryItems ?? []
         for param in parameters {
-            if let index = items.firstIndex(where: { $0.name == param.key }) {
-                items[index] = URLQueryItem(name: param.key, value: param.value)
+            if let index = queryItems.firstIndex(where: { $0.name == param.key }) {
+                queryItems[index] = URLQueryItem(name: param.key, value: param.value)
             } else {
-                items.append(URLQueryItem(name: param.key, value: param.value))
+                queryItems.append(URLQueryItem(name: param.key, value: param.value))
             }
         }
-        components.queryItems = items
-        return components.url ?? self
+        urlComponents.queryItems = queryItems
+        return urlComponents.url!
+    }
+    
+    mutating func myAppendQueryParameters(_ parameters: [String: String]) {
+        self = myAppendingQueryParameters(parameters)
     }
     
     func deletingAllQueryParameters() -> URL {
-        guard var components = URLComponents.init(url: self, resolvingAgainstBaseURL: true) else { return self }
-        components.queryItems = nil
-        return components.url ?? self
+        var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: true)!
+        urlComponents.queryItems = nil
+        return urlComponents.url!
+    }
+    
+    mutating func deleteAllQueryParameters() {
+        self = deletingAllQueryParameters()
+    }
+    
+    func myAppendingQueryParameters(_ parameters: [URLQueryItem]) -> URL {
+        var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: false)!
+        var queryItems = urlComponents.queryItems ?? []
+        for param in parameters {
+            if let index = queryItems.firstIndex(where: { $0.name == param.name }) {
+                queryItems[index] = param
+            } else {
+                queryItems.append(param)
+            }
+        }
+        urlComponents.queryItems = queryItems
+        return urlComponents.url!
+    }
+    
+    mutating func myAppendQueryParameters(_ parameters: [URLQueryItem]) {
+        self = myAppendingQueryParameters(parameters)
     }
     
 }
