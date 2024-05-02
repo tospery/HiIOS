c89e9fb71490bd70e846d7cf0bb325b2522c4b66
tospery <tospery@gmail.com>
2024-05-02 22:39:09 +0800

    download

--- HiIOS/Core/ImageSource.swift
+++ HiIOS/Core/ImageSource.swift
@@ -6,12 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
-
-//public enum ImageSourceType {
-//    case url
-//    case image
-//}
 
 public protocol ImageSource {
 }
@@ -19,30 +13,3 @@ public protocol ImageSource {
 extension URL: ImageSource {}
 extension UIImage: ImageSource {}
 
-
-public func download(
-    with source: ImageSource?,
-    options: KingfisherOptionsInfo? = nil,
-    alwaysTemplate: Bool = false,
-    completion: ((UIImage?, HiError?) -> Void)? = nil
-) {
-    if source == nil {
-        completion?(nil, nil)
-        return
-    }
-    if let image = source as? UIImage {
-        completion?(alwaysTemplate ? image.template : image, nil)
-        return
-    }
-    
-    if let url = source as? URL {
-        ImageDownloader.default.downloadImage(with: url, options: options) { result in
-            switch result {
-            case .success(let image):
-                completion?(image.image, nil)
-            case .failure(let error):
-                completion?(nil, error.asHiError)
-            }
-        }
-    }
-}
