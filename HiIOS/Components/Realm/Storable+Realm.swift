44bc4197018904ca2706d561baa6a5e7d2258994
tospery <tospery@gmail.com>
2024-05-12 09:30:50 +0800

    storable

---
+++ HiIOS/Components/Realm/Storable+Realm.swift
@@ -0,0 +1,78 @@
+//
+//  Storable+Realm.swift
+//  HiIOS
+//
+//  Created by liaoya on 2022/7/19.
+//
+
+import Foundation
+import ObjectMapper
+import RealmSwift
+
+public extension Storable {
+
+    static func objectKey(id: String? = nil) -> String {
+        var key = String(fullname: self)
+        if let path = id {
+            key += "#\(path)"
+        }
+        return key
+    }
+
+    static func arrayKey(page: String? = nil) -> String {
+        var key = "\(String(fullname: self))List"
+        if let path = page {
+            key += "#\(path)"
+        }
+        return key
+    }
+    
+    static func storeObject(_ object: Self?, id: String? = nil) {
+//        let key = self.objectKey(id: id)
+//        if let object = object {
+//            try? archiver.transformCodable(ofType: self).setObject(object, forKey: key)
+//        } else {
+//            try? archiver.removeObject(forKey: key)
+//        }
+    }
+    
+    static func storeArray(_ array: [Self]?, page: String? = nil) {
+//        let key = self.arrayKey(page: page)
+//        if let array = array {
+//            try? archiver.transformCodable(ofType: [Self].self).setObject(array, forKey: key)
+//        } else {
+//            try? archiver.removeObject(forKey: key)
+//        }
+    }
+
+    static func cachedObject(id: String? = nil) -> Self? {
+//        let key = self.objectKey(id: id)
+//        if let object = try? archiver.transformCodable(ofType: self).object(forKey: key) {
+//            return object
+//        }
+//        if let path = Bundle.main.path(forResource: key, ofType: "json"),
+//            let json = try? String(contentsOfFile: path, encoding: .utf8) {
+//            return Self(JSONString: json)
+//        }
+        return nil
+    }
+
+    static func cachedArray(page: String? = nil) -> [Self]? {
+//        let key = self.arrayKey(page: page)
+//        if let array = try? archiver.transformCodable(ofType: [Self].self).object(forKey: key) {
+//            return array
+//        }
+//        if let path = Bundle.main.path(forResource: key, ofType: "json"),
+//            let json = try? String(contentsOfFile: path, encoding: .utf8) {
+//            return [Self](JSONString: json)
+//        }
+        return nil
+    }
+
+    static func eraseObject(id: String? = nil) {
+//        let key = self.objectKey(id: id)
+//        try? archiver.removeObject(forKey: key)
+    }
+
+}
+
